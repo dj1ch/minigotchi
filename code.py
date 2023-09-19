7f7e4982abc073d8e036e8aa5265d483cd5ce6df
dj1ch <tg.1ch0712@gmail.com>
2023-09-19 10:08:05 -0700

    more progress(yay)

--- code.py
+++ code.py
@@ -5,3 +5,12 @@ import adafruit_requests as requests
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 from adafruit_esp32spi import adafruit_esp32spi
 from secrets import secrets
+# this is based on the guide https://www.tomshardware.com/how-to/get-wi-fi-internet-on-raspberry-pi-pico
+print("Minigotchi by dj1ch")
+# esp objects
+esp32_cs = DigitalInOut(board.GP13)
+esp32_ready = DigitalInOut(board.GP14)
+esp32_reset = DigitalInOut(board.GP15)
+# spi object
+spi = busio.SPI(board.GP10, board.GP11, board.GP12)
+#
