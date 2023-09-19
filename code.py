3b46e5e9839f049d1d2a5889cac09cfc3a8afc79
dj1ch <tg.1ch0712@gmail.com>
2023-09-19 11:53:56 -0700

    removed useless stuff.

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
-# to do: add nodemcu firmware, add adafruit stuff, add luapico for firmware to work
-# this is based on the guide https://www.tomshardware.com/how-to/get-wi-fi-internet-on-raspberry-pi-pico
+# Imports here
+#
 print("Minigotchi by dj1ch")
 
 #
