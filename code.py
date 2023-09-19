a4e4ef7f474ec7e0ee95246c1ff0699dad905060
dj1ch <tg.1ch0712@gmail.com>
2023-09-19 10:03:46 -0700

    importing modules

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
-# to soon be added
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+from secrets import secrets
