8a87bfeb0552aae3813e916c19b7c371e1d28649
dj1ch <tg.1ch0712@gmail.com>
2023-09-19 11:53:10 -0700

    removed stuff

--- code.py
+++ code.py
@@ -1,17 +1,5 @@
 # to do: add nodemcu firmware, add adafruit stuff, add luapico for firmware to work
-import board
-import busio
-from digitalio import DigitalInOut
-import adafruit_requests as requests
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi
-from secrets import secrets
 # this is based on the guide https://www.tomshardware.com/how-to/get-wi-fi-internet-on-raspberry-pi-pico
 print("Minigotchi by dj1ch")
-# esp objects
-esp32_cs = DigitalInOut(board.GP13)
-esp32_ready = DigitalInOut(board.GP14)
-esp32_reset = DigitalInOut(board.GP15)
-# spi object
-spi = busio.SPI(board.GP10, board.GP11, board.GP12)
+
 #
