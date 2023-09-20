884bf1a0221faee143f40c7099a16a32e27418a5
dj1ch <tg.1ch0712@gmail.com>
2023-09-20 11:52:38 -0700

    more stuff added to code

--- code.py
+++ code.py
@@ -1,9 +1,18 @@
 # As of right now, this is just a template for the code. Waiting for actual hardware so I can test.
 # Imports here
-import board 
+import board
+import network
 # Start
 print("Minigotchi by dj1ch")
 # Starts wifi card and monitor mode
+try:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(False)  
+    wlan.config(mode=network.WLAN.MODE_MONITOR)
+    wlan.active(True)
+    print("Wi-Fi interface in monitor mode")
+except Exception as e:
+    print("Error:", e)
 
 # Associates and sends packets along network
 
