b6b67501ebf4ad397c02bc8c62e7eb2ad15f50e7
dj1ch <tg.1ch0712@gmail.com>
2023-09-19 21:42:11 -0700

    updated

--- code.py
+++ code.py
@@ -1,5 +1,10 @@
+# As of right now, this is just a template for the code. Waiting for actual hardware so I can test. 
+
 # Imports here
-#
+# Start
 print("Minigotchi by dj1ch")
+# Starts wifi card and monitor mode
+
+# Associates and sends packets along network
 
 #
