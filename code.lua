d6139110a0b3dae7caa61ec25e3875c36318df7d
dj1ch <tg.1ch0712@gmail.com>
2023-09-21 21:12:12 -0700

    some light progress here.

--- code.lua
+++ code.lua
@@ -1,1 +1,12 @@
--- Nothing here as of right now...
+--Maybe some progress here??
+function wifi.monitor.start()
+    print("Wi-Fi monitoring started")
+end
+
+function wifi.monitor.channel(channelNumber)
+    print("Switched to channel " .. channelNumber)
+end
+
+-- executing the functions...
+wifi.monitor.start()
+wifi.monitor.channel(6)
