f1e73479329e9f130404ba9e544136de62d9c53d
dj1ch <tg.1ch0712@gmail.com>
2023-09-22 14:36:38 -0700

    variable channel name

--- code.lua
+++ code.lua
@@ -3,6 +3,10 @@ function wifi.monitor.start()
     print("Wi-Fi monitoring started")
 end
 
+-- let's just assume that the pwnagotchi will start on channel 6 on bootup
+local channelNumber
+channelNumber = 6
+
 function wifi.monitor.channel(channelNumber)
     print("Switched to channel " .. channelNumber)
 end
