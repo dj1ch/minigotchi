f67d40ac2d211bb3a1fafdb7b97c3cf52c468762
dj1ch <tg.1ch0712@gmail.com>
2023-09-24 10:43:57 -0700

    we don't need that comment, we need to save disk space. making comments shorter

--- code.lua
+++ code.lua
@@ -1,16 +1,12 @@
---Maybe some progress here??
+-- functions
 function wifi.monitor.start()
     print("Wi-Fi monitoring started")
 end
-
--- let's just assume that the pwnagotchi will start on channel 6 on bootup
---local channelNumber
---channelNumber = 6
 
 function wifi.monitor.channel(channelNumber)
     print("Switched to channel " .. channelNumber)
 end
 
--- executing the functions...
+-- execute functions
 wifi.monitor.start()
 wifi.monitor.channel(6)
