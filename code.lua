220aa2d37307a93c4b34603be4a5a5686571f02a
dj1ch <tg.1ch0712@gmail.com>
2023-09-27 11:51:08 -0700

    kind of functional i think?

--- code.lua
+++ code.lua
@@ -1,3 +1,7 @@
+-- load modules
+wifi = require("wifi")
+local message = "123"
+
 -- functions
 function wifi.monitor.start()
     print("Wi-Fi monitoring started")
@@ -7,6 +11,15 @@ function wifi.monitor.channel(channelNumber)
     print("Switched to channel " .. channelNumber)
 end
 
+function sendBroadcastPacket()
+    local packet = wifi.packet()
+    packet.frame_type = 0x00
+    packet.frame_body = message
+    packet:broadcast()
+    print("Broadcasted: " .. message)
+end
+
 -- execute functions
 wifi.monitor.start()
 wifi.monitor.channel(6)
+sendBroadcastPacket()
