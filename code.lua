198a1018cf6b4a1e81b0199662ddd2382fcf43ef
dj1ch <tg.1ch0712@gmail.com>
2023-09-28 11:04:15 -0700

    i need to start fetching the branches before doing this.

--- code.lua
+++ code.lua
@@ -4,16 +4,22 @@ local message = "123"
 
 -- functions
 function wifi.monitor.start()
+    wifi.monitor.start
     print("Wi-Fi monitoring started")
 end
 
 function wifi.monitor.channel(channelNumber)
+    wifi.monitor.channel(channelNumber)
     print("Switched to channel " .. channelNumber)
 end
 
-function sendBroadcastPacket()
+function associate
+-- put the stuff here
+end
+
+function pwnagotchipacket()
     local packet = wifi.packet()
-    packet.frame_type = 0x00
+    packet.frame_type = 0x08
     packet.frame_body = message
     packet:broadcast()
     print("Broadcasted: " .. message)
@@ -22,4 +28,4 @@ end
 -- execute functions
 wifi.monitor.start()
 wifi.monitor.channel(6)
-sendBroadcastPacket()
+pwnagotchipacket()
