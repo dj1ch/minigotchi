4e7cdb48d3d46db68adcfba25636b949216607e0
dj1ch <tg.1ch0712@gmail.com>
2023-09-29 08:40:32 -0700

    configuring uart

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # Turns on light to indicate it is on
+from machine import UART
+import machine
+import _thread
+import time
+from machine import UART
 from machine import Pin, Timer
 led = Pin(25, Pin.OUT)
 led.toggle()
 
 # Startup
 print("Minigotchi by dj1ch")
-print("\nRun the script by typing 'lua <filename here>'.")
 print("\nYou can turn off the light on the pico by running 'led.toggle()' in the terminal here.")
+
+uart = UART(1,115200)
+print('-- UART Serial --')
+print('>', end='')
