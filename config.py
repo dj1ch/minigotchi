6d52b6855d8e4214b734b1adadf408e4db726685
dj1ch <tg.1ch0712@gmail.com>
2023-09-30 23:34:06 -0700

    comments in config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    STATE_PIN_ID = 22
-    RESET_PIN_ID = 20
+    STATE_PIN_ID = 22 # will identify later
+    RESET_PIN_ID = 20 # will identify later, we will go with this for now.
     UART_ID = 0
     UART_BAUDRATE = 115200
