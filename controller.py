58655d1929aae4d3f859bc68526c616227acedea
dj1ch <tg.1ch0712@gmail.com>
2023-10-02 08:57:28 -0700

    i may need these back now.

--- controller.py
+++ controller.py
@@ -11,6 +11,17 @@ class Controller:
         self.wifi_ssid = "" # Must be set
         self.wifi_password = "" # Must be set
 
+    def server_requests_handler(self, request_data):
+        pass
+
+    def start_server(self):
+        print("Starting server at port 8080")
+
+        if self.esp.start_server(8080):
+            print("Server running...")
+
+            self.esp.server_mainloop(self.server_requests_handler)
+
     def esp_as_client(self):
         print(f"Connecting to network: {self.wifi_ssid}")
         self.esp.connect_to_network(self.wifi_ssid, self.wifi_password)
@@ -19,8 +30,21 @@ class Controller:
             print(f"Address data: {self.esp.get_address_as_client()}")
 
     def send_requests(self):
-        get_data = self.esp.send_get("127.0.0.1", "/get", 8666) # 127.0.0.1 is localhost, so idk 
+        get_data = self.esp.send_get("127.0.0.1", "/get", 8666) # 127.0.0.1 is localhost, so idk
         post_data = self.esp.send_post(str({'key': 'value'}), {}, "127.0.0.1", "/post", 8666)
 
         print(f"POST data: {post_data}")
         print(f"GET data: {get_data}")
+
+    def mainloop(self):
+        print("Starting ESP8266...")
+        self.esp.startup()
+
+        if self.esp.check_module():
+            print("ESP8266 running...")
+
+            self.esp_as_client()
+            # self.esp_as_host()
+
+            # self.send_requests()
+            self.start_server()
