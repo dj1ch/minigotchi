2a53a9c1a32d64fbeeff04a99bf87932ba622cd2
dj1ch <tg.1ch0712@gmail.com>
2023-10-02 09:22:29 -0700

    oops

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# turns on light to indicate it is on
-import time
-from controller import Controller
-from machine import Pin, Timer
-led = Pin(25, Pin.OUT)
-led.toggle()
-
-# startup
-print("-- Minigotchi by dj1ch --")
-print("\nYou can turn off the light on the pico by running 'led.toggle()' in the terminal here.")
-
 # setup the server, associating, etc
-def setup()
+def setup():
     controller = Controller()
     controller.__init__()
     print("Starting in client mode...")
@@ -23,20 +12,18 @@ def setup()
     print(" ")
 
 # sending get and post requests to specified ip
-def get_and_post(max_duration_seconds)
+def get_and_post(max_duration_seconds):
     start_time = time.time()
-    print("\fSending requests...")
-    controller.send_requests()
-    print("done.")
-    print(" ")
-    # check for time
-    elapsed_time = time.time() - start_time
+    while True:
+        elapsed_time = time.time() - start_time
         if elapsed_time >= max_duration_seconds:
             break
-
-max_duration = 60
-my_function(max_duration)
+        print("\fSending requests...")
+        controller.send_requests()
+        print("done.")
+        print(" ")
 
 # run the script
 setup()
-get_and_post() # set how long these packets should be sent here
+max_duration = 60
+get_and_post(max_duration)
