f6f1c96fe87d1b9775131c0a88982d43f82b5428
dj1ch <tg.1ch0712@gmail.com>
2023-10-06 08:54:13 -0700

    cmake stuff for now

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from machine import Pin
 led = Pin(25, Pin.OUT)
 led.toggle()
 
+# note this will run the first thing you plug it in
 print("-- Minigotchi by dj1ch --")
 print("\nYou can turn off the light on the pico by running 'led.toggle()' in the terminal here.")
