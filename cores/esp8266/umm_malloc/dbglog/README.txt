e10cfbd39ca96f1fb8cfcb8967918f4bb6119ec7
dj1ch <tg.1ch0712@gmail.com>
2023-10-11 09:01:43 -0700

    removing folders, these libraries will be provided in the ide.

--- cores/esp8266/umm_malloc/dbglog/README.txt
+++
@@ -1,2 +0,0 @@
-Downloaded from: https://github.com/rhempel/c-helper-macros/tree/develop
-Applied uncrustify to be consistent with the rest of the umm_malloc files.
--- tests/ci/build_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# CI job to run the documentation build
-
-set -ev
-
-root=$(git rev-parse --show-toplevel)
-env SPHINXOPTS="-W" make -C $root/doc html
