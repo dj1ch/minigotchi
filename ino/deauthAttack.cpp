bf3ad673fc1295ff97686fa3f5f1e64fafd6b612
dj1ch <tg.1ch0712@gmail.com>
2023-11-07 20:54:57 -0800

    this is a cpp, not a .h

--- ino/deauthAttack.cpp
+++ ino/deauthAttack.cpp
@@ -1,5 +1,7 @@
 #include "deauth.h"
 
+DeauthAttack deauthAttack;  // Instantiate the deauthAttack object
+
 void DeauthAttack::selectRandomAP() {
     int apCount = WiFi.scanNetworks();
 
@@ -27,4 +29,9 @@ void DeauthAttack::startRandomDeauth() {
     } else {
         Serial.println("No access point selected. Use selectRandomAP() first.");
     }
+}
+
+void DeauthAttack::start(bool param1, bool param2, bool param3, bool param4, bool param5, int param6) {
+    // Implementation of the start function
+    // Modify this according to your requirements
 }
