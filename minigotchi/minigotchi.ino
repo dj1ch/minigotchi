64df0acb4fcd797b05a1218fc29339193a0feae4
dj1ch <tg.1ch0712@gmail.com>
2023-11-25 15:39:20 -0800

    allowed scanning to stop for deauthing, or payload

--- minigotchi/minigotchi.ino
+++ minigotchi/minigotchi.ino
@@ -3,28 +3,37 @@
 #include "pwnagotchi.h"
 #include "deauth.h"
 #include "packet.h"
+#include "raw80211.h"
 
 Pwnagotchi pwnagotchi;
 PacketSender packetSender;
 DeauthAttack deauthAttack;
+Raw80211 raw;
 
 void setup() {
     Serial.begin(115200);
+    Serial.println(" ");
     Serial.println("Hi, I'm Minigotchi, your pwnagotchi's best friend!");
     Serial.println(" ");
     Serial.println("You can edit my whitelist in the minigotchi.ino, and you can also edit the json parameters in the packet.cpp");
     Serial.println(" ");
+    Serial.println("Starting now...");
     deauthAttack.addToWhitelist("SSID"); // add your ssid(s) here
     deauthAttack.addToWhitelist("ANOTHER_SSID");
-    delay(5000);
+    raw.init("bssid of ap you will listen on", channel number); // set the settings here, ("BSSID", channel)
+    raw.start();
+    delay(15000);
     Serial.println(" ");
-    Serial.println("Starting now...");
+    Serial.println("Started successfully!");
 }
 
 void loop() {
     // get local payload from local pwnagotchi
     pwnagotchi.detectAndHandlePwnagotchi();
     delay(5000);
+
+    // stop for deauthing and payload
+    raw.stop();
 
     // send payload
     packetSender.sendJsonPayload();
@@ -34,4 +43,7 @@ void loop() {
     deauthAttack.selectRandomAP();
     deauthAttack.startRandomDeauth();
     delay(5000);
+
+    // restart the process
+    raw.start();
 }
