af425b013451e20125aab37965f963525e62bd51
dj1ch <tg.1ch0712@gmail.com>
2023-12-30 14:40:36 -0800

    fixed spacing

--- minigotchi/webui.cpp
+++ minigotchi/webui.cpp
@@ -1,3 +1,20 @@
 ///////////////////////////////////
 // webui.cpp: handles the web ui //
 ///////////////////////////////////
+
+#include "webui.h"
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer server(80);
+
+WebUI::WebUI() {
+    server.on("/", HTTP_GET, std::bind(&WebUI::handleRoot, this));
+}
+
+void WebUI::setupWebServer() {
+    
+}
+
+void WebUI::handleRoot() {
+  
+}
--- minigotchi/webui.h
+++ minigotchi/webui.h
@@ -1,3 +1,34 @@
 /////////////////////////////////////////
 // webui.h: header files for webui.cpp //
 /////////////////////////////////////////
+
+#ifndef WEBUI_H
+#define WEBUI_H
+
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+class WebUI {
+public:
+    WebUI();
+    void setupWebServer();
+
+private:
+    const char* _htmlContent = R"=====(
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Minigotchi by dj1ch</title>
+    </head>
+    <body>
+        <h1>Hello from minigotchi!</h1>
+        <p></p>
+        <p>This is a webserver being ran on the esp8266.</p>
+    </body>
+    </html>
+    )=====";
+
+    void handleRoot();
+};
+
+#endif // WEBUI_H
