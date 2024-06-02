866dbaa5b7edb5e437e78fcb369f40c860983587
dj1ch <tg.1ch0712@gmail.com>
2024-06-01 17:12:46 -0700

    epoch

--- minigotchi/minigotchi.ino
+++ minigotchi/minigotchi.ino
@@ -1,6 +1,6 @@
 /**
  * minigotchi.ino: everything implemented here
-*/
+ */
 
 #include "config.h"
 #include "minigotchi.h"
@@ -13,14 +13,18 @@ void setup() {
     minigotchi.boot();
 }
 
-/** developer note: 
+/** developer note:
  *
  * everything should have been moved to minigotchi.cpp
  * as the program becomes more complicated, it gets easier to maintain all the main features in one file
- * 
-*/
+ *
+ */
 
 void loop() {
+    // current epoch
+    minigotchi.epoch();
+    delay(250);
+
     // cycle channels at start of loop
     minigotchi.cycle();
     delay(250);
