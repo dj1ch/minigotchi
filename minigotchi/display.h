8b9dd80d84c3a66772072154782ee1e9a512d5f2
dj1ch <tg.1ch0712@gmail.com>
2024-06-09 23:13:34 -0700

    ssd1305 support

--- minigotchi/display.h
+++ minigotchi/display.h
@@ -7,6 +7,8 @@
 
 #include "config.h"
 #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1305.h>
+#include <U8g2lib.h>
 #include <Adafruit_SSD1306.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -15,8 +17,18 @@
 #define SSD1306_SCREEN_WIDTH 128
 #define SSD1306_SCREEN_HEIGHT 64
 
+#define SSD1305_SCREEN_WIDTH 128
+#define SSD1305_SCREEN_HEIGHT 32
+
+#define SSD1305_OLED_CLK 14
+#define SSD1305_OLED_MOSI 13
+#define SSD1305_OLED_CS 15
+#define SSD1305_OLED_DC 4
+#define SSD1305_OLED_RESET 5
+
 // Init screen
 #define SSD1306_OLED_RESET -1
+#define SSD1305_OLED_RESET -1
 #define WEMOS_OLED_SHIELD_OLED_RESET 0 // GPIO0
 
 class Display {
