484909c93241f8250c329d3836d5a6a3058f7fd0
Restyled.io <commits@restyled.io>
2024-06-22 04:53:49 +0000

    Restyled by clang-format

--- minigotchi/config.h
+++ minigotchi/config.h
@@ -14,46 +14,46 @@
 
 class Config {
 public:
-    static bool deauth;
-    static bool advertise;
-    static int shortDelay;
-    static int longDelay;
-    static bool parasite;
-    static bool display;
-    static std::string screen;
-    static int baud;
-    static int channel;
-    static std::vector<std::string> whitelist;
-    static int epoch;
-    static std::string grid_version;
-    static std::string face;
-    static std::string identity;
-    static std::string name;
-    static int ap_ttl;
-    static bool associate;
-    static int bored_num_epochs;
-    static int channels[13];
-    static int excited_num_epochs;
-    static int hop_recon_time;
-    static int max_inactive_scale;
-    static int max_interactions;
-    static int max_misses_for_recon;
-    static int min_recon_time;
-    static int min_rssi;
-    static int recon_inactive_multiplier;
-    static int recon_time;
-    static int sad_num_epochs;
-    static int sta_ttl;
-    static int pwnd_run;
-    static int pwnd_tot;
-    static std::string session_id;
-    static int timestamp;
-    static int uptime;
-    static std::string version;
+  static bool deauth;
+  static bool advertise;
+  static int shortDelay;
+  static int longDelay;
+  static bool parasite;
+  static bool display;
+  static std::string screen;
+  static int baud;
+  static int channel;
+  static std::vector<std::string> whitelist;
+  static int epoch;
+  static std::string grid_version;
+  static std::string face;
+  static std::string identity;
+  static std::string name;
+  static int ap_ttl;
+  static bool associate;
+  static int bored_num_epochs;
+  static int channels[13];
+  static int excited_num_epochs;
+  static int hop_recon_time;
+  static int max_inactive_scale;
+  static int max_interactions;
+  static int max_misses_for_recon;
+  static int min_recon_time;
+  static int min_rssi;
+  static int recon_inactive_multiplier;
+  static int recon_time;
+  static int sad_num_epochs;
+  static int sta_ttl;
+  static int pwnd_run;
+  static int pwnd_tot;
+  static std::string session_id;
+  static int timestamp;
+  static int uptime;
+  static std::string version;
 
 private:
-    static int random(int min, int max);
-    static int time();
+  static int random(int min, int max);
+  static int time();
 };
 
 #endif // CONFIG_H
--- minigotchi/frame.cpp
+++ minigotchi/frame.cpp
@@ -39,7 +39,7 @@ const uint8_t Frame::SignatureAddr[] = {0xde, 0xad, 0xbe, 0xef, 0xde, 0xad};
 const uint8_t Frame::BroadcastAddr[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 const uint16_t Frame::wpaFlags = 0x0411;
 
-const uint8_t Frame::header[] {
+const uint8_t Frame::header[]{
     /*  0 - 1  */ 0x80,
     0x00, // frame control, beacon frame
     /*  2 - 3  */ 0x00,
@@ -100,146 +100,147 @@ const int Frame::pwngridHeaderLength = sizeof(Frame::header);
  */
 
 void Frame::pack() {
-    // make a json doc
-    String jsonString = "";
-    DynamicJsonDocument doc(2048);
+  // make a json doc
+  String jsonString = "";
+  DynamicJsonDocument doc(2048);
 
-    doc["epoch"] = Config::epoch;
-    doc["face"] = Config::face;
-    doc["identity"] = Config::identity;
-    doc["name"] = Config::name;
+  doc["epoch"] = Config::epoch;
+  doc["face"] = Config::face;
+  doc["identity"] = Config::identity;
+  doc["name"] = Config::name;
 
-    JsonObject policy = doc.createNestedObject("policy");
-    policy["advertise"] = Config::advertise;
-    policy["ap_ttl"] = Config::ap_ttl;
-    policy["associate"] = Config::associate;
-    policy["bored_num_epochs"] = Config::bored_num_epochs;
+  JsonObject policy = doc.createNestedObject("policy");
+  policy["advertise"] = Config::advertise;
+  policy["ap_ttl"] = Config::ap_ttl;
+  policy["associate"] = Config::associate;
+  policy["bored_num_epochs"] = Config::bored_num_epochs;
 
-    JsonArray channels = policy.createNestedArray("channels");
-    for (size_t i = 0; i < sizeof(Config::channels) / sizeof(Config::channels[0]);
-            ++i) {
-        channels.add(Config::channels[i]);
+  JsonArray channels = policy.createNestedArray("channels");
+  for (size_t i = 0; i < sizeof(Config::channels) / sizeof(Config::channels[0]);
+       ++i) {
+    channels.add(Config::channels[i]);
+  }
+
+  policy["deauth"] = Config::deauth;
+  policy["excited_num_epochs"] = Config::excited_num_epochs;
+  policy["hop_recon_time"] = Config::hop_recon_time;
+  policy["max_inactive_scale"] = Config::max_inactive_scale;
+  policy["max_interactions"] = Config::max_interactions;
+  policy["max_misses_for_recon"] = Config::max_misses_for_recon;
+  policy["min_recon_time"] = Config::min_rssi;
+  policy["min_rssi"] = Config::min_rssi;
+  policy["recon_inactive_multiplier"] = Config::recon_inactive_multiplier;
+  policy["recon_time"] = Config::recon_time;
+  policy["sad_num_epochs"] = Config::sad_num_epochs;
+  policy["sta_ttl"] = Config::sta_ttl;
+
+  doc["pwnd_run"] = Config::pwnd_run;
+  doc["pwnd_tot"] = Config::pwnd_tot;
+  doc["session_id"] = Config::session_id;
+  doc["uptime"] = Config::uptime;
+  doc["version"] = Config::version;
+
+  // serialize then put into beacon frame
+  serializeJson(doc, jsonString);
+  Frame::essidLength = measureJson(doc);
+  Frame::headerLength = 2 + ((uint8_t)(essidLength / 255) * 2);
+
+  size_t newLength =
+      Frame::pwngridHeaderLength + Frame::essidLength + Frame::headerLength;
+  memcpy(Frame::beaconFrame, Frame::header, Frame::pwngridHeaderLength);
+
+  /** developer note:
+   *
+   * if you literally want to check the json everytime you send a packet(non
+   * serialized ofc)
+   *
+   * Serial.println(jsonString);
+   */
+
+  int currentByte = pwngridHeaderLength;
+
+  for (int i = 0; i < Frame::essidLength; i++) {
+    if (i == 0 || i % 255 == 0) {
+      Frame::beaconFrame[currentByte++] = Frame::IDWhisperPayload;
+      if (Frame::essidLength - i < Frame::chunkSize) {
+        Frame::payloadSize = Frame::essidLength - i;
+      }
+      Frame::beaconFrame[currentByte++] = Frame::payloadSize;
     }
 
-    policy["deauth"] = Config::deauth;
-    policy["excited_num_epochs"] = Config::excited_num_epochs;
-    policy["hop_recon_time"] = Config::hop_recon_time;
-    policy["max_inactive_scale"] = Config::max_inactive_scale;
-    policy["max_interactions"] = Config::max_interactions;
-    policy["max_misses_for_recon"] = Config::max_misses_for_recon;
-    policy["min_recon_time"] = Config::min_rssi;
-    policy["min_rssi"] = Config::min_rssi;
-    policy["recon_inactive_multiplier"] = Config::recon_inactive_multiplier;
-    policy["recon_time"] = Config::recon_time;
-    policy["sad_num_epochs"] = Config::sad_num_epochs;
-    policy["sta_ttl"] = Config::sta_ttl;
-
-    doc["pwnd_run"] = Config::pwnd_run;
-    doc["pwnd_tot"] = Config::pwnd_tot;
-    doc["session_id"] = Config::session_id;
-    doc["uptime"] = Config::uptime;
-    doc["version"] = Config::version;
-
-    // serialize then put into beacon frame
-    serializeJson(doc, jsonString);
-    Frame::essidLength = measureJson(doc);
-    Frame::headerLength = 2 + ((uint8_t)(essidLength / 255) * 2);
-
-    size_t newLength = Frame::pwngridHeaderLength + Frame::essidLength + Frame::headerLength;
-    memcpy(Frame::beaconFrame, Frame::header, Frame::pwngridHeaderLength);
-
-    /** developer note:
-     *
-     * if you literally want to check the json everytime you send a packet(non
-     * serialized ofc)
-     *
-     * Serial.println(jsonString);
-     */
-
-    int currentByte = pwngridHeaderLength;
-
-    for (int i = 0; i < Frame::essidLength; i++) {
-        if (i == 0 || i % 255 == 0) {
-            Frame::beaconFrame[currentByte++] = Frame::IDWhisperPayload;
-            if (Frame::essidLength - i < Frame::chunkSize) {
-                Frame::payloadSize = Frame::essidLength - i;
-            }
-            Frame::beaconFrame[currentByte++] = Frame::payloadSize;
-        }
-
-        uint8_t nextByte = (uint8_t)'?';
-        if (isAscii(jsonString[i])) {
-            nextByte = (uint8_t)jsonString[i];
-        }
-
-        Frame::beaconFrame[currentByte++] = nextByte;
+    uint8_t nextByte = (uint8_t)'?';
+    if (isAscii(jsonString[i])) {
+      nextByte = (uint8_t)jsonString[i];
     }
 
-    /* Uncomment if you want to test beacon frames
+    Frame::beaconFrame[currentByte++] = nextByte;
+  }
 
-    Serial.println("('-') Full Beacon Frame:");
-    for (size_t i = 0; i < sizeof(Frame::beaconFrame); ++i) {
-      Serial.print(beaconFrame[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.println(" ");
+  /* Uncomment if you want to test beacon frames
 
-    */
+  Serial.println("('-') Full Beacon Frame:");
+  for (size_t i = 0; i < sizeof(Frame::beaconFrame); ++i) {
+    Serial.print(beaconFrame[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+
+  */
 }
 
 bool Frame::send() {
-    // build frame
-    Frame::pack();
-    size_t totalLength = Frame::pwngridHeaderLength + Frame::essidLength + Frame::headerLength;
+  // build frame
+  Frame::pack();
+  size_t totalLength =
+      Frame::pwngridHeaderLength + Frame::essidLength + Frame::headerLength;
 
-    // send full frame
-    // we dont use raw80211 since it sends a header(which we don't need), although
-    // we do use it for monitoring, etc.
-    Frame::sent =
-        wifi_send_pkt_freedom(Frame::beaconFrame, totalLength, 0);
-    delay(102);
-    return (Frame::sent == 0);
+  // send full frame
+  // we dont use raw80211 since it sends a header(which we don't need), although
+  // we do use it for monitoring, etc.
+  Frame::sent = wifi_send_pkt_freedom(Frame::beaconFrame, totalLength, 0);
+  delay(102);
+  return (Frame::sent == 0);
 }
 
 void Frame::advertise() {
-    int packets = 0;
-    unsigned long startTime = millis();
+  int packets = 0;
+  unsigned long startTime = millis();
 
-    if (Config::advertise) {
-        Serial.println("(>-<) Starting advertisment...");
-        Serial.println(" ");
-        Display::updateDisplay("(>-<)", "Starting advertisment...");
-        Minigotchi::monStart();
-        Parasite::sendAdvertising();
-        delay(Config::shortDelay);
-        for (int i = 0; i < 150; ++i) {
-            if (Frame::send()) {
-                packets++;
+  if (Config::advertise) {
+    Serial.println("(>-<) Starting advertisment...");
+    Serial.println(" ");
+    Display::updateDisplay("(>-<)", "Starting advertisment...");
+    Minigotchi::monStart();
+    Parasite::sendAdvertising();
+    delay(Config::shortDelay);
+    for (int i = 0; i < 150; ++i) {
+      if (Frame::send()) {
+        packets++;
 
-                // calculate packets per second
-                float pps = packets / (float)(millis() - startTime) * 1000;
+        // calculate packets per second
+        float pps = packets / (float)(millis() - startTime) * 1000;
 
-                // show pps
-                if (!isinf(pps)) {
-                    Serial.print("(>-<) Packets per second: ");
-                    Serial.print(pps);
-                    Serial.print(" pkt/s (Channel: ");
-                    Serial.print(Channel::getChannel());
-                    Serial.println(")");
-                    Display::updateDisplay(
-                        "(>-<)", "Packets per second: " + (String)pps + " pkt/s" +
-                        "(Channel: " + (String)Channel::getChannel() + ")");
-                }
-            } else {
-                Serial.println("(X-X) Advertisment failed to send!");
-            }
+        // show pps
+        if (!isinf(pps)) {
+          Serial.print("(>-<) Packets per second: ");
+          Serial.print(pps);
+          Serial.print(" pkt/s (Channel: ");
+          Serial.print(Channel::getChannel());
+          Serial.println(")");
+          Display::updateDisplay(
+              "(>-<)", "Packets per second: " + (String)pps + " pkt/s" +
+                           "(Channel: " + (String)Channel::getChannel() + ")");
         }
-
-        Serial.println(" ");
-        Serial.println("(^-^) Advertisment finished!");
-        Serial.println(" ");
-        Display::updateDisplay("(^-^)", "Advertisment finished!");
-    } else {
-        // do nothing but still idle
+      } else {
+        Serial.println("(X-X) Advertisment failed to send!");
+      }
     }
+
+    Serial.println(" ");
+    Serial.println("(^-^) Advertisment finished!");
+    Serial.println(" ");
+    Display::updateDisplay("(^-^)", "Advertisment finished!");
+  } else {
+    // do nothing but still idle
+  }
 }
